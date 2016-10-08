28e039d5872d14db8ba70bc121d8938ad2c72f5c
hemanth@blitsy.com <hemanth@blitsy.com>
2016-10-08 14:14:02 -0500

    Did some more stuff

--- src/com/company/FileList.java
+++ src/com/company/FileList.java
@@ -1,7 +1,12 @@
 package com.company;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Date;
 
 /**
  * Created by Hemanth on 10/5/16.
@@ -10,6 +15,26 @@ public class FileList {
     private String htmlCode="";
     private String currDir=System.getProperty("user.dir");
 
+public void setCurrDir(String dir){
+    currDir = currDir + dir;
+    System.out.println(currDir);
+
+}
+
+public byte[] getFile(String fileName){
+    try {
+
+
+        Path path = Paths.get(currDir);
+        System.out.println(path.toAbsolutePath().toString());
+        byte[] data = Files.readAllBytes(path);
+        return data;
+    }catch (IOException e){
+        System.out.println("Unable to get file");
+        return "File not Found".getBytes();
+    }
+
+}
 public String getHtmlCode(){
     setFiles();
     return htmlCode;
@@ -19,25 +44,89 @@ public void setFiles(){
     File f1 = new File(currDir);
     File[] strFileDirs = f1.listFiles();
     for (int i =0;i< strFileDirs.length;i++){
-        if (strFileDirs[i].isDirectory())
-        System.out.println("Directory: " + strFileDirs[i].toString());
-        else if(strFileDirs[i].isFile())
-            //System.out.println("File: " + strFileDirs[i].toString());
-
-            htmlCode = htmlCode + formatHtmlFile(strFileDirs[i].getName());
+        if (strFileDirs[i].isDirectory()) {
+            System.out.println("Directory: " + strFileDirs[i].toString());
+            htmlCode = htmlCode + formatHtmlFile(strFileDirs[i], "DIR");
             System.out.println(htmlCode);
+        }
+        else if(strFileDirs[i].isFile()) {
+            //System.out.println("File: " + strFileDirs[i].toString());
+            //System.out.println(strFileDirs[i].getAbsolutePath());
+            //getMIMEtype(strFileDirs[i].getName());
+            htmlCode = htmlCode + formatHtmlFile(strFileDirs[i], getMIMEtype(strFileDirs[i].getName()));
+            System.out.println(htmlCode);
+        }
 
     }
+
 
 }
 
-    public String formatHtmlFile(String input){
+    public String formatHtmlFile(File input, String mimeType){
+        Date lastModified = new Date(input.lastModified());
+        int spaceing =5;
 
-        return "<img src=\"/icons/text.gif\" alt=\"[TXT]\"> <a href=\""+input+"\">"+input+"</a>\n";
+        return "<img src=\"\" alt=\"["+mimeType.split("/")[0]+"]\">  <a href=\""+input.getName()+"\">"+input.getName()+"</a>"+"           "+lastModified.toString()+"     "
+                  +input.length()+"\n";
+
+    }
+    public String formatHtmlHeader(){
+
+        return "<h1>Index of " + currDir + "</h1>";
 
     }
 
+    public String getCurrDir(){
+        return this.currDir;
+    }
 
+//
+//    public File sendFile(String fileName){
+//        System.out.println("Received File:");
+//        return new File();
+//    }
+    private String getSpace(String input,int space){
+        int length = input.length();
+        System.out.println("Length: " + length);
+        String returnString ="";
+        if (length < space) {
+            for (int i =0;i <= space-length;i++){
+                returnString = returnString + " ";
+            }
+        }
+        //System.out.println("ReturnString is: " + returnString);
+        return returnString;
+
+
+
+    }
+    public String getMIMEtype(String fileName){
+//        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") !=0)
+//            fileName.substring(fileName.lastIndexOf(".")+1);
+//        System.out.println("File EXT: " + fileName);
+        String[] ext = fileName.split("\\.");
+        String extCheck = ext[ext.length-1];
+        switch (extCheck){
+            case "txt":
+                return "text/plain";
+            case "java":
+                return "text/plain";
+            case "mp3":
+                return "audio/mpeg3";
+            case "jpeg":
+                return "image/jpeg";
+            case "jpg":
+                return "image/jpeg";
+            case "png":
+                return "image/png";
+            case "html":
+                return "text/html";
+            default :
+                return "text/plain";
+
+        }
+
+    }
 
 
 }
--- src/com/company/FileServer.java
+++ src/com/company/FileServer.java
@@ -1,7 +1,9 @@
 package com.company;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
 
 /**
  * Created by Hemanth on 10/5/16.
@@ -10,28 +12,27 @@ public class FileServer implements Runnable {
     Socket sock;
     FileServer(Socket s){sock=s;}
     private BufferedReader in =null;
-    private PrintWriter out = null;
+    private OutputStream out = null;
+    FileList hello = new FileList();
+
 
     @Override
     public void run(){
         try {
             System.out.println("File Server");
             in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            out = new PrintWriter(sock.getOutputStream());
-            System.out.println(in.readLine());
-//            while (in.readLine() != null ){
-//                System.out.println(in.readLine());
-//                System.out.println("a");
-//            }
-            FileList hello = new FileList();
-            String message = "<p>Hello</p>";
-            System.out.println("hello");
-            out.println("HTTP/1.1 200 OK");
-            out.println("Content-Type: text/html");
-            out.println("Content-Length: "+hello.getHtmlCode().length());
-            out.print("\r\n");
-            System.out.println(hello.getHtmlCode());
-            out.println(hello.getHtmlCode());
+            out = (sock.getOutputStream()) ;
+//            System.out.println(in.readLine());
+//                if (in.readLine().isEmpty()){
+//                    errorView(out);
+//                    return;
+//                }
+            String enteredString = in.readLine();
+            System.out.println(enteredString);
+            String[] identifyInput = enteredString.split(" ");
+            System.out.println(Arrays.toString(identifyInput));
+            chooser(identifyInput[1]);
+            //basicView(out);
             out.flush();
             out.close();
         }catch (IOException e){
@@ -40,6 +41,111 @@ public class FileServer implements Runnable {
         try {
             sock.close();
         }catch (IOException e){System.out.println(e.getMessage());}
+    }
+
+
+    private void basicView(OutputStream out){
+        String listingHeader = "<pre><img src=\"\" alt=\"Icon \"> <a href=\"?C=N;O=D\">Name</a>                    <a href=\"?C=M;O=A\">Last modified</a>                       <a href=\"?C=S;O=A\">Size</a>     <a href=\"?C=D;O=A\">Description</a><hr><img src=\"\" alt=\"[DIR]\"> <a href=\""+hello.getCurrDir()+"\">Parent Directory</a>                             -   \n";
+        String sendHtml = hello.getHtmlCode();
+        String formatHeader = hello.formatHtmlHeader();
+        System.out.println("Current Dir: "+hello.getCurrDir());
+        int length = listingHeader.length()+sendHtml.length()+formatHeader.length()+9;
+        try {
+            out.write("HTTP/1.1 200 OK".getBytes());
+            out.write("Content-Type: text/html".getBytes());
+            out.write(("Content-Length: " + length).getBytes());
+            System.out.println("Content-Length: " + length);
+            out.write("\r\n".getBytes());
+            out.write(formatHeader.getBytes());
+            out.write(listingHeader.getBytes());
+            out.write(sendHtml.getBytes());
+            out.write("</pre>".getBytes());
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+    private void folderView(OutputStream out,String folder){
+        System.out.println("Get Folder: " + folder);
+        hello.setCurrDir(folder);
+        String listingHeader = "<pre><img src=\"\" alt=\"Icon \"> <a href=\"?C=N;O=D\">Name</a>                    <a href=\"?C=M;O=A\">Last modified</a>                       <a href=\"?C=S;O=A\">Size</a>     <a href=\"?C=D;O=A\">Description</a><hr><img src=\"\" alt=\"[DIR]\"> <a href=\""+hello.getCurrDir()+"\">Parent Directory</a>                             -   \n";
+        String sendHtml = hello.getHtmlCode();
+        String formatHeader = hello.formatHtmlHeader();
+        int length = listingHeader.length()+sendHtml.length()+formatHeader.length()+9+9999999;
+        System.out.println("Current Dir: "+hello.getCurrDir());
+        try {
+            out.write("HTTP/1.1 200 OK".getBytes());
+            out.write("Content-Type: text/html".getBytes());
+            out.write(("Content-Length: " + length).getBytes());
+            System.out.println("Content-Length: " + length);
+            out.write("\r\n".getBytes());
+            out.write(formatHeader.getBytes());
+            out.write(listingHeader.getBytes());
+//            out.write(sendHtml.getBytes());
+            out.write("</pre>".getBytes());
+            //out.flush();
+        }catch (IOException e){
+            System.out.println(e.getMessage().toString());
+        }
+    }
+
+    private void getfile(OutputStream out,String fileName){
+        System.out.println("Get File: " + fileName);
+        hello.setCurrDir(fileName);
+        //String listingHeader = "<pre><img src=\"\" alt=\"Icon \"> <a href=\"?C=N;O=D\">Name</a>                    <a href=\"?C=M;O=A\">Last modified</a>                       <a href=\"?C=S;O=A\">Size</a>     <a href=\"?C=D;O=A\">Description</a><hr><img src=\"\" alt=\"[DIR]\"> <a href=\""+hello.getCurrDir()+"\">Parent Directory</a>                             -   \n";
+        //String sendHtml = hello.getHtmlCode();
+        //String formatHeader = hello.formatHtmlHeader();
+        //int length = listingHeader.length()+sendHtml.length()+formatHeader.length()+9;
+        byte[] data = hello.getFile(fileName);
+        System.out.println(hello.getMIMEtype(fileName.substring(fileName.lastIndexOf("/") + 1)));
+        try {
+
+            out.write("HTTP/1.1 200 OK".getBytes());
+            out.write(("Content-Type: " + hello.getMIMEtype(fileName.substring(fileName.lastIndexOf("/") + 1))).getBytes());
+            out.write(("Content-Length: " + data.length).getBytes());
+            System.out.println("Content-Length: " + data.length);
+            out.write("\r\n".getBytes());
+            System.out.println(data);
+            out.write(data);
+            //out.println(formatHeader);
+            //out.println(listingHeader);
+            //out.println(sendHtml);
+            //out.println("</pre>");
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private void chooser(String input){
+        System.out.println(input);
+       if (input.equals("/"))
+           basicView(out);
+       else if (input.contains("/") && input.contains(".")){
+           getfile(out,input);
+       }
+       else if (input.contains("/")){
+           folderView(out,input);
+       }
+        else{
+           errorView(out);
+       }
+
+
+
+    }
+
+    public void errorView(OutputStream out){
+        String error = "<h1>500 Error</h1>";
+        try {
+            out.write("HTTP/1.1 500".getBytes());
+            out.write("Content-Type: text/html".getBytes());
+            out.write((("Content-Length: " + error.length()).getBytes()));
+            out.write("\r\n".getBytes());
+            out.write(error.getBytes());
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 
 }
